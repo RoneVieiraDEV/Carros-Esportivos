80ecf7353ab6db12ed1e389db1956cf7a3457ba1
RoneVieiraDEV <ronevieira66@gmail.com>
2024-09-09 18:28:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+      titulo: "Ford Mustang",
+      descricao: "O Ford Mustang é muito mais do que apenas um carro; ele é um símbolo da cultura americana, do poder e da liberdade. Desde seu lançamento em 1964, o Mustang conquistou o coração de milhões de apaixonados por automóveis ao redor do mundo.",
+      link: "https://en.wikipedia.org/wiki/Ford_Mustang",
+      tags: "ford mustang carro"
+  },
+  {
+      titulo: "Jaguar F-Type",
+      descricao: "O Jaguar F-Type é um carro esportivo que evoca a tradição e o luxo britânico, combinado com um desempenho emocionante. Desde seu lançamento, ele tem encantado os entusiastas de automóveis com seu design elegante, motor potente e experiência de condução envolvente.",
+      link: "https://en.wikipedia.org/wiki/Jaguar_F-Type",
+      tags: "jaguar f type carro esportivo"
+  },
+  {
+      titulo: "BMW Série 5",
+      descricao: "A BMW Série 5 é um dos modelos mais icônicos da marca bávara, reconhecido por sua combinação de luxo, performance e tecnologia. Direcionado para o segmento executivo, o Série 5 oferece um equilíbrio perfeito entre conforto e esportividade, atraindo tanto empresários quanto entusiastas de automóveis.",
+      link: "https://pt.wikipedia.org/wiki/BMW_S%C3%A9rie_5",
+      tags: "bmw serie 5  carro"
+  }
+];
